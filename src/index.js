204c43d4e91ae3c073ddef0d7a4c7457bb42013a
Yauheni Dubouski <joinhud@gmail.com>
2018-02-24 00:03:05 +0300

    quadratic-equation/feature: implement business logic for solvin quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let matches = equation.match(/^(-?\d+) \* x\^2 ([-\+]{1}) (\d+) \* x ([-\+]{1}) (\d+)$/);
+  let a = +matches[1];
+  let b = +(matches[2] + matches[3]);
+  let c = +(matches[4] + matches[5]);
+  let discriminant = (b * b) - 4 * (a * c);
+  let sqrtDiscriminant = Math.sqrt(discriminant);
+  let x1 = Math.round((- b - sqrtDiscriminant) / (2 * a));
+  let x2 = Math.round((- b + sqrtDiscriminant) / (2 * a));
+
+  return a < 0 ? [x2, x1] : [x1, x2];
 }
